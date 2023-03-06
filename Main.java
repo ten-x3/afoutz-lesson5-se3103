60742ebc8e964ff27f16e4e99da662e56c4d7ca6
ten <tenxdaneai@gmail.com>
2023-03-06 12:04:00 -0600

    design pattern notes

--- Main.java
+++ Main.java
@@ -1,1 +1,3 @@
-// design patterns
+// design patterns: general reusable solution to commonly occuring problem
+//                 - not a finished design, but can be transformed into source code
+//                 - more like a template
